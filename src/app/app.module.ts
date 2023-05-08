4990208e36c50275ccb98c7b90e7f9ab25ebfe34
Edward Lale <edward.gratefuldead1@gmail.com>
2023-05-08 00:39:51 -0500

    Working on prayer request list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ListPrayerRequestsComponent } from './list-prayer-requests/list-prayer-requests.component';
-import { ListCategoriesComponent } from './list-categories/list-categories.component';
-import { ListBookmarksComponent } from './list-bookmarks/list-bookmarks.component';
-import { ListAnalyticsComponent } from './list-analytics/list-analytics.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ListPrayerRequestsComponent} from './list-prayer-requests/list-prayer-requests.component';
+import {ListCategoriesComponent} from './list-categories/list-categories.component';
+import {ListBookmarksComponent} from './list-bookmarks/list-bookmarks.component';
+import {ListAnalyticsComponent} from './list-analytics/list-analytics.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -21,9 +23,12 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/list-prayer-requests/list-prayer-requests.component.ts
+++ src/app/list-prayer-requests/list-prayer-requests.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PrayerServiceService } from '../services/prayer-service.service';
-import { PrayerRequests } from '../models/prayer-requests.model';
-import { PrayerRequestCategory } from '../models/prayer-request-category.model'
-import { Categories } from '../models/categories.model';
-import { PrayerRequestDTO } from '../models/prayer-request-dto.model';
-import { User } from '../models/user.model';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PrayerServiceService} from '../services/prayer-service.service';
+import {PrayerRequests} from '../models/prayer-requests.model';
+import {PrayerRequestCategory} from '../models/prayer-request-category.model'
+import {Categories} from '../models/categories.model';
+import {PrayerRequestDTO} from '../models/prayer-request-dto.model';
+import {User} from '../models/user.model';
 
 @Component({
   selector: 'app-list-prayer-requests',
   templateUrl: './list-prayer-requests.component.html',
   styleUrls: ['./list-prayer-requests.component.css']
 })
-export class ListPrayerRequestsComponent implements OnInit{
+export class ListPrayerRequestsComponent implements OnInit {
   prayerRequestDTO: PrayerRequestDTO[] = [];
   prayerRequests: PrayerRequests[] = [];
   prayerRequestCategories: PrayerRequestCategory[] = [];
@@ -20,8 +20,9 @@ export class ListPrayerRequestsComponent implements OnInit{
   users: User[] = [];
   selectedPrayerRequest: PrayerRequestDTO | null = null;
   prayerRequest: PrayerRequestDTO[] = [];
-  
-  constructor(private route: ActivatedRoute, private service: PrayerServiceService) {}
+
+  constructor(private route: ActivatedRoute, private service: PrayerServiceService) {
+  }
 
   ngOnInit() {
 
@@ -38,6 +39,13 @@ export class ListPrayerRequestsComponent implements OnInit{
     prayerRequest.isSelected = true;
     this.selectedPrayerRequest = prayerRequest;
     console.log('selectedPrayerRequest', this.selectedPrayerRequest);
+  }
+
+  onCloseDetails(prayerRequest: { selectedPrayerRequest: PrayerRequestDTO | null }): void {
+    if (this.selectedPrayerRequest != null) {
+      this.selectedPrayerRequest.isSelected = false;
+    }
+    console.log("selectedPrayerRequest", prayerRequest);
   }
 }
 
